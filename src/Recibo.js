2bd8f2fc70d04497e42ad2c4cbdfb5e31f736e61
gleiderr <gleiderr@gmail.com>
2020-01-11 19:57:35 -0300

    Estilização

--- src/Recibo.js
+++ src/Recibo.js
@@ -1,29 +1,70 @@
 import React from 'react';
 import './Recibo.css';
+import cabeçalho from './images/cabecalho.jpeg';
 
 function Recibo() {
+    const data = new Date();
+    const ano = data.getFullYear();
+    const mes = data.getMonth() + 1;
+    const dia = data.getDay();
+    const hoje = `${ano}-${mes < 10 ? '0' + mes : mes}-${
+        dia < 10 ? '0' + dia : dia
+    }`;
+
+    console.log(hoje);
+
     return (
         <>
-            <input type='file' name='imagem' />
+            <input type='file' name='imagem' style={{ display: 'none' }} />
 
-            <label for='nome'>Cliente</label>
-            <input type='text' name='nome' id='nome' />
+            <div style={{ width: '100%' }}>
+                <img
+                    style={{ width: '100%' }}
+                    src={cabeçalho}
+                    alt='Imagem do Cabeçalho'
+                />
+            </div>
 
-            <label for='endereço'>Endereço</label>
-            <input type='text' name='endereço' id='endereço' />
+            <div style={{ display: 'flex' }}>
+                <label htmlFor='nome'>Cliente: </label>
+                <input type='text' name='nome' id='nome' style={{ flex: 1 }} />
+            </div>
 
-            <label for='telefone'>Telefone:</label>
-            <input type='tel' name='telefone' id='telefone' />
+            <div style={{ display: 'flex' }}>
+                <label htmlFor='endereço'>Endereço: </label>
+                <input
+                    type='text'
+                    name='endereço'
+                    id='endereço'
+                    style={{ flex: 1 }}
+                />
+            </div>
 
-            <input type='date' name='data' id='data' />
+            <div style={{ display: 'flex' }}>
+                <label htmlFor='telefone'>Telefone: </label>
+                <input
+                    type='tel'
+                    name='telefone'
+                    id='telefone'
+                    style={{ flex: 1 }}
+                />
+
+                <input type='date' name='data' id='data' defaultValue={hoje} />
+            </div>
 
             <table>
+                <colgroup>
+                    <col style={{ width: '10%' }} />
+                    <col />
+                    <col style={{ width: '20%' }} />
+                    <col style={{ width: '20%' }} />
+                </colgroup>
                 <thead>
                     <tr>
-                        <th>Quantidade</th>
+                        <th className='number-format'>Qtd.</th>
                         <th>Descrição</th>
-                        <th>Valor Unitário</th>
-                        <th>Valor Total</th>
+                        <th className='number-format'>Valor Unitário</th>
+                        <th className='number-format'>Valor Total</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,11 +78,15 @@ function Recibo() {
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td>Total Geral</td>
+                        <th
+                            colSpan='3'
+                            scope='row'
+                            style={{ textAlign: 'right' }}
+                        >
+                            Total Geral
+                        </th>
                         <td>
-                            <input />
+                            <input className='number-format' />
                         </td>
                     </tr>
                 </tfoot>
@@ -54,16 +99,16 @@ function Linha() {
     return (
         <tr>
             <td>
-                <input type='text' />
+                <input type='text' className='number-format' />
             </td>
             <td>
                 <input type='text' />
             </td>
             <td>
-                <input type='text' />
+                <input type='text' className='number-format' />
             </td>
             <td>
-                <input type='text' />
+                <input type='text' className='number-format' />
             </td>
         </tr>
     );
